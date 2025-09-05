06102e85f66fc21bdf55d385160930fb2b0bb1cb
Leonardo <leonardomartinslopes2@gmail.com>
2025-09-05 18:33:12 -0300

    [changed] Alteracao para exercicio de arquivo binario

--- exercicio3.c
+++ exercicio3.c
@@ -3,22 +3,65 @@
 #include <string.h>
 
 int main(){
-    char texto[1000];
-    char *token;
-    unsigned int num_colunas = 0;
+    typedef struct {
+        int codigo;
+        char nome[50];
+        float preco;
+    } Produto;
 
-    printf("Digite o texto:");
-    fgets(texto, sizeof(texto), stdin);
+    int opcao;
+    do{
+        printf("Digite 1 para cadastrar novos produtos, 2 para listar os produtos e 3 para sair\n");
+        scanf("%d", &opcao);
 
-    texto[strcspn(texto, "\n")] = '\0';
+        switch (opcao){
+        case 1: {
+            Produto prod;
+            printf("Digite o codigo:\n");
+            scanf("%d", &prod.codigo);
 
-    token = strtok(texto, ",");
-    num_colunas++;
-    while (token != NULL){
-        printf("%2u: Frase = %s\n", num_colunas, token);
-        num_colunas++;
-        token = strtok(NULL, ",");
-    }
+            printf("Digite o nome:\n");
+            fgetc(stdin); // remove o '\n' sobrando no buffer
+            fgets(prod.nome, sizeof(prod.nome), stdin);
+            prod.nome[strcspn(prod.nome, "\n")] = 0; // remove o '\n' do final
+
+            printf("Digite o preco:\n");
+            scanf("%f", &prod.preco);
+            
+            FILE *arquivo = fopen("produtos.bin", "ab");
+            if (!arquivo) {
+                perror("Erro ao abrir arquivo");
+                break;
+            }
+
+            fwrite(&prod, sizeof(Produto), 1, arquivo);
+            fclose(arquivo);
+
+            break;
+        }
+        case 2: {
+            Produto prod;
+            FILE *arq = fopen("produtos.bin", "rb");
+            if (!arq) {
+                perror("Erro ao abrir arquivo");
+                break;
+            }
+
+            while (fread(&prod, sizeof(Produto), 1, arq) == 1){
+                printf("Codigo:%6d, Nome: %s, Preço: %.2f\n", prod.codigo, prod.nome, prod.preco);
+            }
+            fclose(arq);
+
+            break;
+        }
+        case 3:
+            printf("Saindo do programa...\n");
+            break;
+        default:
+            printf("Opcao invalida!\n");
+            break;
+        }
+    } while(opcao != 3);
 
     return 0;
 }
