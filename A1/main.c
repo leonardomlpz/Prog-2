e56b32cc55b0d97142c8b779731be40b422b5475
Leonardo <leonardomartinslopes2@gmail.com>
2025-10-08 18:42:06 -0300

    [modified] Modificacao estetica do main

--- A1/main.c
+++ A1/main.c
@@ -10,7 +10,6 @@ int main(int argc, char *argv[]) {
         printf("Uso: %s <opção> <biblioteca> [documentos...]\n", argv[0]);
         return 1;
     }
-
     const char *opcao = argv[1];
     const char *biblioteca = argv[2];
 
@@ -22,37 +21,53 @@ int main(int argc, char *argv[]) {
             return 1;
         }
     }
-
     if (gbv_open(&lib, biblioteca) != 0) {
         printf("Erro ao abrir biblioteca %s\n", biblioteca);
         return 1;
     }
 
-    if (strcmp(opcao, "-a") == 0) {
-        for (int i = 3; i < argc; i++) {
-            gbv_add(&lib, biblioteca, argv[i]);
+    if (opcao[0] == '-' && strlen(opcao) == 2) {
+        switch (opcao[1]) {
+            case 'a':
+                for (int i = 3; i < argc; i++) {
+                    gbv_add(&lib, biblioteca, argv[i]);
+                }
+                break;
+
+            case 'r':
+                for (int i = 3; i < argc; i++) {
+                    gbv_remove(&lib, argv[i]);
+                }
+                break;
+
+            case 'l':
+                gbv_list(&lib);
+                break;
+
+            case 'v':
+                if (argc < 4) {
+                    fprintf(stderr, "Erro: A opção '-v' requer o nome de um documento.\n");
+                } else {
+                    gbv_view(&lib, argv[3]);
+                }
+                break;
+
+            case 'o':
+                if (argc < 4) {
+                    fprintf(stderr, "Erro: A opção '-o' requer um critério.\n");
+                } else {
+                    gbv_order(&lib, biblioteca, argv[3]);
+                }
+                break;
+
+            default:
+                printf("Opção '-%c' é inválida.\n", opcao[1]);
+                break;
         }
-
-
-    } else if (strcmp(opcao, "-r") == 0) {
-        for (int i = 3; i < argc; i++) {
-            gbv_remove(&lib, argv[i]);
-        }
-
-    } else if (strcmp(opcao, "-l") == 0) {
-        gbv_list(&lib);
-
-    } else if (strcmp(opcao, "-v") == 0 && argc >= 4) {
-        gbv_view(&lib, argv[3]);
-
-    } else if (strcmp(opcao, "-o") == 0 && argc >= 4) {
-        gbv_order(&lib, biblioteca, argv[3]);
-        
     } else {
-        printf("Opção inválida.\n");
+        printf("Formato de opção inválido: '%s'. Use opções como '-a', '-l', etc.\n", opcao);
     }
 
     gbv_close();
     return 0;
 }
-
